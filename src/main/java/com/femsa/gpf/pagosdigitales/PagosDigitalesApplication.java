c257afa6acf968e1d9eda22634873729dcac28da
Jesús Guerra <jguerra.1991@outlook.com>
2025-11-24 12:08:41 -0500

    Initial commit

---
+++ src/main/java/com/femsa/gpf/pagosdigitales/PagosDigitalesApplication.java
@@ -0,0 +1,13 @@
+package com.femsa.gpf.pagosdigitales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagosDigitalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagosDigitalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/femsa/gpf/pagosdigitales/PagosDigitalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.femsa.gpf.pagosdigitales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagosDigitalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
